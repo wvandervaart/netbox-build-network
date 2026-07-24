d0155b58ad1e53531bbfcf73a5696c6d8f8108ee
Wouter van der Vaart <wv@peakfactory.com>
2026-07-24 12:06:32 +0200

    Revert "Replace auto window.open() with a manual target=_blank link"
    This reverts commit 1daf3c446114f73dd1653b6b5a4655f3457a5769.

--- netbox_build_network/views.py
+++ netbox_build_network/views.py
@@ -1,23 +1,27 @@
+import json
 from urllib.parse import urlencode
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.http import HttpResponse
-from django.utils.html import escape
 from django.views import View
 from netbox.plugins.utils import get_plugin_config
 
 _PAGE_TEMPLATE = """<!DOCTYPE html>
 <html>
-<head><title>Build Network</title></head>
+<head><title>Sending buildnw&hellip;</title></head>
 <body>
-<p><a href="{buildnw_url}" target="_blank" rel="noopener noreferrer">Send buildnw request</a></p>
+<script>
+  window.open({buildnw_url}, '_blank', 'noopener,noreferrer');
+  window.location.replace({back_url});
+</script>
+<p>Buildnw sent in a new window.</p>
 </body>
 </html>
 """
 
 
 class BuildnwRedirectView(PermissionRequiredMixin, View):
-    """Presents a link that opens the configured buildnw_url in a new tab with message=<user>."""
+    """Opens the configured buildnw_url in a new tab, triggering a GET with message=<user>."""
 
     permission_required = 'netbox_build_network.send_buildnw'
 
@@ -26,6 +30,10 @@ class BuildnwRedirectView(PermissionRequiredMixin, View):
         message = request.user.get_username()
         query = urlencode({'message': message})
         buildnw_url = f'{base_url}?{query}'
+        back_url = request.META.get('HTTP_REFERER') or '/'
 
-        html = _PAGE_TEMPLATE.format(buildnw_url=escape(buildnw_url))
+        html = _PAGE_TEMPLATE.format(
+            buildnw_url=json.dumps(buildnw_url),
+            back_url=json.dumps(back_url),
+        )
         return HttpResponse(html)
